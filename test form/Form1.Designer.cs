a191c93af234d17107401cb69e456791d1cf421f
VLADYKA-derf <31366490+VLADYKA-derf@users.noreply.github.com>
2017-11-25 11:48:30 +0300

    Initial commit

---
+++ test form/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace test_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(138, 82);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 8);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(138, 8);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(144, 69);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(339, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(113, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(339, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(113, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(306, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(458, 8);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(75, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 143);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ test form/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace test_form
+{
+    public partial class Form1 : Form
+    {
+        string[] com = { "процессор", "видеокарта", "оперативная память", "материнская плата" };
+        string[] cup = { "Intel Xeon E5-2679", "Intel Xeon E5-2699", "Intel Xeon E5-2699" };
+        string[] video = { "GeForce GTX TITAN Z", "Radeon R9 295X2", "GeForce GTX 1080" };
+        string[] memory = { "2", "4", "8", "12" };
+        string[] motherboard = { "ASRock FM2A68M-HD+", "ASUS Z170-DELUXE", "ASUS PRIME X299-A", "MSI X299 TOMAHAWK" };
+        Int32[] prise_cup;
+        Int32[] prise_video;
+        Int32[] prise_memory;
+        Int32[] prise_motherboard;
+        Int32 count = 0;
+        DirectoryInfo di = new DirectoryInfo("DataTime");
+        string path_cup = @"DataTime\\cup.txt";
+        string path_video = @"DataTime\\video.txt";
+        string path_memory = @"DataTime\\memory.txt";
+        string path_motherboard = @"DataTime\\motherboard.txt";
+        string path_prise_cup = @"DataTime\\prise_cup.txt";
+        string path_prise_video = @"DataTime\\prise_video.txt";
+        string path_prise_memory = @"DataTime\\prise_memory.txt";
+        string path_prise_motherboard = @"DataTime\\prise_motherboard.txt";
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.AddRange(com);
+            comboBox2.Items.AddRange(com);
+            comboBox1.Text = "комплектующие";
+            comboBox2.Text = "выберите тип";
+            label1.Text = "Цена:";
+            button1.Text = "добавить";
+            listBox1.Update();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                di.Create();
+                StreamWriter sw_cup = new StreamWriter(path_cup, true); sw_cup.Close();
+                StreamWriter sw_video = new StreamWriter(path_video, true); sw_video.Close();
+                StreamWriter sw_memory = new StreamWriter(path_memory, true); sw_memory.Close();
+                StreamWriter sw_motherboard = new StreamWriter(path_motherboard, true); sw_motherboard.Close();
+                StreamWriter sw_prise_cup = new StreamWriter(path_prise_cup, true); sw_prise_cup.Close();
+                StreamWriter sw_prise_video = new StreamWriter(path_prise_video, true); sw_prise_video.Close();
+                StreamWriter sw_prise_memory = new StreamWriter(path_prise_memory, true); sw_prise_memory.Close();
+                StreamWriter sw_prise_motherboard = new StreamWriter(path_prise_motherboard, true); sw_prise_motherboard.Close();
+                cup = File.ReadAllLines(path_cup);
+                video = File.ReadAllLines(path_video);
+                memory = File.ReadAllLines(path_memory);
+                motherboard = File.ReadAllLines(path_motherboard);
+                prise_cup = sortTxt(path_prise_cup);
+                prise_video = sortTxt(path_prise_video);
+                prise_memory = sortTxt(path_prise_memory);
+                prise_motherboard = sortTxt(path_prise_motherboard);
+                Console.WriteLine("функция");
+                for (int i = 0; i < prise_cup.Length; i++)
+                {
+                    Console.WriteLine(prise_cup[i]);
+                }
+                for (int i = 0; i < cup.Length; i++)
+                {
+                    Console.WriteLine(cup[i]);
+                }
+
+
+            }
+            catch (Exception)
+            {
+
+               // throw;
+            }
+        }
+        private int[] sortTxt(string path)
+        {
+
+                StreamReader objReader = new StreamReader(path);
+                string sLine = "";
+                List<string> arrText = new List<string>();
+                while (sLine != null)
+                {
+                    sLine = objReader.ReadLine();
+                    if (sLine != null)
+                        arrText.Add(sLine);
+                }
+                objReader.Close();
+                int[] arr = new int[arrText.Count];
+                for (int i = 0; i < arrText.Count; i++)
+                {
+                    arr[i] = Int32.Parse(arrText[i]);
+                }
+                return arr;
+            }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+
+                for (int i = 0; i < cup.Length; i++)
+                    if (listBox1.SelectedIndex == i)
+                    {
+                        listBox2.Items.Add(cup[i]);
+                        listBox1.Items.Clear();
+                        count += prise_cup[i];
+                        break;
+                    }
+                for (int i = 0; i < video.Length; i++)
+                    if (listBox1.SelectedIndex == i)
+                    {
+                        listBox2.Items.Add(video[i]);
+                        listBox1.Items.Clear();
+                        count += prise_video[i];
+                        break;
+                    }
+                for (int i = 0; i < memory.Length; i++)
+                    if (listBox1.SelectedIndex == i)
+                    {
+                        listBox2.Items.Add(memory[i]);
+                        listBox1.Items.Clear();
+                        count += prise_memory[i];
+                        break;
+                    }
+                for (int i = 0; i < motherboard.Length; i++)
+                    if (listBox1.SelectedIndex == i)
+                    {
+                        listBox2.Items.Add(motherboard[i]);
+                        listBox1.Items.Clear();
+                        count += prise_motherboard[i];
+                        break;
+                    }
+                label2.Text = count.ToString();
+            }
+            catch (Exception)
+            {
+
+                // throw;
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    listBox1.Items.AddRange(cup);
+                }
+                if (comboBox1.SelectedIndex == 1)
+                {
+                    listBox1.Items.AddRange(video);
+                }
+                if (comboBox1.SelectedIndex == 2)
+                {
+                    listBox1.Items.AddRange(memory);
+                }
+                if (comboBox1.SelectedIndex == 3)
+                {
+                    listBox1.Items.AddRange(motherboard);
+                }
+            }
+            catch (Exception)
+            {
+                //throw;
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+                StreamWriter sw_cup = new StreamWriter(path_cup, true);
+                StreamWriter sw_video = new StreamWriter(path_video, true);
+                StreamWriter sw_memory = new StreamWriter(path_memory, true);
+                StreamWriter sw_motherboard = new StreamWriter(path_motherboard, true);
+                StreamWriter sw_prise_cup = new StreamWriter(path_prise_cup, true);
+                StreamWriter sw_prise_video = new StreamWriter(path_prise_video, true);
+                StreamWriter sw_prise_memory = new StreamWriter(path_prise_memory, true);
+                StreamWriter sw_prise_motherboard = new StreamWriter(path_prise_motherboard, true);
+                if (comboBox2.SelectedIndex == 0)
+                {
+                    sw_cup.WriteLine(textBox1.Text);
+                    sw_prise_cup.WriteLine(textBox2.Text);
+                    sw_cup.Close();
+                    sw_prise_cup.Close();
+                }
+                if (comboBox2.SelectedIndex == 1)
+                {
+                    sw_video.WriteLine(textBox1.Text);
+                    sw_prise_video.WriteLine(textBox2.Text);
+                    sw_video.Close();
+                    sw_prise_video.Close();
+                }
+                if (comboBox2.SelectedIndex == 2)
+                {
+                    sw_memory.WriteLine(textBox1.Text);
+                    sw_prise_memory.WriteLine(textBox2.Text);
+                    sw_memory.Close();
+                    sw_prise_memory.Close();
+                }
+                if (comboBox2.SelectedIndex == 3)
+                {
+                    sw_motherboard.WriteLine(textBox1.Text);
+                    sw_prise_motherboard.WriteLine(textBox2.Text);
+                    sw_motherboard.Close();
+                    sw_prise_motherboard.Close();
+                }
+        }
+        
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+
